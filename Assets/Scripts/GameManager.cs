9d983eeecfd24cc5e091add5bfefd7257192f8e7
geoni-ara <lgeeckh@gmail.com>
2025-09-17 23:52:21 +0900

    Mobile APK 기준 UI 배치 조정 및 BuildSetting 폰에 설치 되도록 수정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -27,4 +27,9 @@ public class GameManager : MonoBehaviour
             Destroy(enemy.gameObject);
         }
     }
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
 }
